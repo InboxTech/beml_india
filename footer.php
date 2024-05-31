1c96cfadfb7530f32ce3d82d79b554907688be5b
Dipak Gohil <dipakgohil7273@gmail.com>
2024-05-31 14:36:49 +0530

    footer temperory is compeletd.

--- footer.php
+++ footer.php
@@ -1,2 +1,87 @@
+<link rel="stylesheet" href="public/css/style.css">
+<link rel="stylesheet" href="public/bootstrap/bootstrap.min.css">
+
+<footer>
+    <div class="bussiness-details">
+        <ul>
+            <li><label for="title">Tel</label></li>
+            <li><a href="tel:+918022963250">+918022963250</a></li>
+            <li><a href="tel:+918022963200">+918022963200</a></li>
+        </ul>
+        <ul>
+            <li><label for="title">Business Timing</label></li>
+            <li><label for="days">Monday To Friday</label></li>
+            <li><label for="timing">8:30 am to 5:15 pm</label></li>
+        </ul>
+        <ul>
+            <li><label for="title">Address</label></li>
+            <li><label for="number">23/1,4th Main, SR nagar, banglore -560027, karnataka, INDIA</label></li>
+        </ul>
+        <ul>
+            <li><label for="title">CIN No</label></li>
+            <li><label for="number">L53202KA1964GQ01001530</label></li>
+        </ul>
+    </div>
+
+    <div class="send-offers-tag">
+        <div class="tag-text">
+            <label for="">Newsletter</label> </br>
+            <label for="">Be the first one to know about discount, offers and events</label>
+        </div>
+        <div class="send-mail-box">
+            <div class="input-group">
+                <input type="email" class="input" id="Email" name="Email" placeholder="Enter your email" autocomplete="off">
+                <input class="button--submit" value="submit" type="submit">
+            </div>
+            <!-- <input type="text" placeholder="Enter your email"> -->
+            <!-- <input type="submit" name="#" value="submit"> -->
+        </div>
+    </div>
+
+    
+    <div class="link-items">
+        <div class="portal-links1">
+            <h1>PORTAL LINKS</h1>
+            <li><label for="">PRMS -Employee and Executives (Executives who retired prior to 01.01.20007)</label></li>
+            <li><label for="">PRMS -Employee and Executives (Executives who retired prior to 01.01.20007)</label></li>
+        </div>  
+        <div class="portal-links2">
+            <h1>PORTAL LINKS</h1>
+            <li><label for="">Online Complaint System</label></li>
+            <li>Invester</li>
+            <li>Vigilance</li>
+            <li>Contractor Registration</li>
+            <li>Returns</li>
+        </div>  
+        <div class="quick-links">
+            <h1>QUICK LINKS</h1>
+            <li>Help Desk</li>
+            <li>Feedback</li>
+            <li>Swachh Bharat</li>
+            <li>Public Grievances</li>
+            <li>Covid-19 Support</li>
+            <li>Digital payment</li>
+            <li>Kalamandir</li>
+            <li>Citizen's Charter</li>
+        </div> 
+        <div class="info-links1">
+            <h1>INFO LINKS</h1>
+            <li>Online Vender Registration</li>
+            <li>Sub-Contractor Registration</li>
+            <li>Life/Existence Certificate</li>
+            <li>Requirement & Form</li>
+            <li>Contracts Concluded</li>
+            <li>other Important Links</li>
+        </div> 
+        <div class="info-links2">
+            <h1>INFO LINKS</h1>
+            <li>Procurement Projection for 05 yrs</li>
+            <li>Component Details Of MSE Vendors</li>
+            <li>Annual Plan for Procurement MSEs</li>
+        </div> 
+
+    </div>
+    
+</footer>
 </body>
 </html>
