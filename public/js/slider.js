093a4e58b24f7c4f075436487cb2027042f60662
Aadil <aadilshaikh0118@gmail.com>
2024-05-31 15:20:20 +0530

    page section 2 complet.

--- public/js/slider.js
+++ public/js/slider.js
@@ -3,22 +3,18 @@ const progressContent = document.querySelector(".autoplay-progress span");
 var swiper = new Swiper(".mySwiper", {
 spaceBetween: 30,
 centeredSlides: true,
-autoplay: {
-    delay: 2500,
-    disableOnInteraction: false
-},
+// autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false
+// },
 pagination: {
     el: ".swiper-pagination",
     type: "fraction",
 },
-navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-},
-on: {
-    autoplayTimeLeft(s, time, progress) {
-    progressCircle.style.setProperty("--progress", 1 - progress);
-    progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-    }
-}
+// on: {
+//     autoplayTimeLeft(s, time, progress) {
+//     progressCircle.style.setProperty("--progress", 1 - progress);
+//     progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+//     }
+// }
 });
