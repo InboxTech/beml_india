88591df211361275c1d6d709030d2b3ee57c7959
“Dipak <dipakgohil7273@gmail.com>
2024-05-31 23:33:56 +0530

    js error solved.

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,20 +1,23 @@
-const progressCircle = document.querySelector(".autoplay-progress svg");
-const progressContent = document.querySelector(".autoplay-progress span");
+
 var swiper = new Swiper(".mySwiper", {
-spaceBetween: 30,
-centeredSlides: true,
-// autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false
-// },
-pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-},
-// on: {
-//     autoplayTimeLeft(s, time, progress) {
-//     progressCircle.style.setProperty("--progress", 1 - progress);
-//     progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-//     }
-// }
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    }
 });
+
+var swiper = new Swiper(".mySwipersec", {
+    slidesPerView: 10,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+  });
