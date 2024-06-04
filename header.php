3715dd247c194f864d1a9b9490c7fbf760a8f29e
“Dipak <dipakgohil7273@gmail.com>
2024-06-04 14:27:07 +0530

    line added in footer.

--- header.php
+++ header.php
@@ -16,24 +16,24 @@ class Header {
             </head>
             <body>  
                 <div class="fist-row"> 
-                    <div clas="heading-epfo"><span> EPFO </span> CIN:L3520G009127df </div> 
+                    <div clas="heading-epfo"><span> EPFO LETTER </span> CIN:L35202KA1964GOI001530 </div> 
                     <div class="fist-row-items"> 
                         <ul> 
                             <li><a href="#"> </a>Skip to Main Content |</li>
                             <li><a href="#"> </a>Accessibility Option |</li>
                             <li><a href="#"> </a>Screen Reader |</li>
-                            <li><a href="#"> </a> A- |</li>
-                            <li><a href="#"> </a> A+ |</li>
-                            <li><a href="#"> </a> A |</li>
+                            <li><a href="#"> </a>A- |</li>
+                            <li><a href="#"> </a>A+ |</li>
+                            <li><a href="#"> </a>A |</li>
                         </ul>
                     </div>
                 </div>
 
                 <div class="second-row">
-                    <div class="welcome-heading"> Welcome To BEML Limited</div>
+                    <div class="welcome-heading"> Welcome To <span> BEML Limited </span></div>
                     <div class="second-row-items">
                         <ul>
-                            <li><a href="#">RTI<img src="public/images/down_arrow.png"></a></li>
+                            <li><a href="#">RTI <img src="public/images/down_arrow.png"> </a></li>
                             <li><a href="#">CSR</a></li>
                             <li><a href="#">CAREERS</a></li>
                             <li><a href="#">TENDER</a></li>
@@ -49,12 +49,15 @@ class Header {
                     <div class="prduct-btn"><a href="#"><img src="public/images/brow_btn.png"></a></div>
                     <div class="search-bar"><input type="text" name="search" placeholder="Search Here..."><img src="public/images/search.png"></div>
                     <div class="adv-image"><img src="public/images/adv_img.png"></div>
-                    <div class="languages">Language<img src="public/images/down_arrow.png"><img src="public/images/flag.webp"></div>
+                    <div class="languages">
+                        <label >Language<img src="public/images/down_arrow.png"> </label>
+                        <span> <img src="public/images/flag-temp.png"> <b> English </b> </span>
+                    </div>
                 </div>
             
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <div class="container-fluid">
-                        <ul class="navbar-nav">
+                        <ul class="navbar-nav mainnav">
                             <li class="nav-item"><a href="#"> Home </a></li> 
                             <li class="nav-item"><a href="#"> Corporate <img src="public/images/down_arrow.png"></a></li> 
                             <li class="nav-item"><a href="#"> Business <img src="public/images/down_arrow.png"></a></li> 
